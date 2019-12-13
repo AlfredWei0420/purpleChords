a70ef212b1315d521daf9d89d24a7cf14105b746
AlexanderZhao <zhaomengzhe@qq.com>
2019-12-13 18:25:26 +0800

    develop basic UI and interact

---
+++ miniprogram/lib/chords.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    "id": "c",
+    "name": "C",
+    "level-name": "I",
+    "chord": [
+      "c4",
+      "e4",
+      "g4"
+    ]
+  },
+  {
+    "id": "cm",
+    "name": "Cm",
+    "level-name": "I",
+    "chord": [
+      "c4",
+      "d4s",
+      "g4"
+    ]
+  },
+  {
+    "id": "c7",
+    "name": "C7",
+    "level-name": "I",
+    "chord": [
+      "c4",
+      "e4",
+      "g4",
+      "a4s"
+    ]
+  },
+  {
+    "id": "cmaj7",
+    "name": "Cmaj7",
+    "level-name": "I",
+    "chord": [
+      "c4",
+      "e4",
+      "g4",
+      "b4"
+    ]
+  }
+]
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,64 @@
 //index.js
 const app = getApp()
 
-Page({})
+const CHORDS = require('../../lib/chords.js')
+
+function playNote(note) {
+  const innerAudioContext = wx.createInnerAudioContext()
+  innerAudioContext.src = `audio/piano/${note}.mp3`
+
+  innerAudioContext.play()
+}
+
+Page({
+  data: {
+    currentKeys: ['c', 'cm', 'c7', 'cmaj7'],
+    currentChordIndex: null,
+    tempo: 70,
+    isPlaying: false
+  },
+  onLoad: function () {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  onKeyPress: function (e) {
+    const key = e.currentTarget.id
+    playNote(key)
+  },
+  onTempoChange: function (e) {
+    this.setData({
+      tempo: e.detail.value
+    })
+  },
+  onPlay: function () {
+    const {
+      currentKeys
+    } = this.data
+
+    for (let index in currentKeys) {
+      setTimeout(() => {
+        const {
+          chord: notes
+        } = CHORDS.find(c => c.id === currentKeys[index])
+
+        notes.forEach(note => {
+          playNote(note)
+        })
+      }, 1000 * index)
+    }
+  }
+})
